2ab1bdea3c919ad60cb34ad206269ce1357b2295
Balsam Louhichi <Balsam.Louhichi@esprit.tn>
2026-02-12 15:46:40 +0100

    Delete sahty_sym directory

--- sahty_sym/src/Controller/hello_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-
-export default class extends Controller {
-  connect() {
-    this.element.innerHTML = "✅ Symfony UX fonctionne !";
-    console.log("Stimulus OK");
-  }
-}
